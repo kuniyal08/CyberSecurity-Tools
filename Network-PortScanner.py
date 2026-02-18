e1876226463a368384b4843f87a18d2d4153a634
Kartikeya Uniyal <kartikeyauniyal@protonmail.com>
2026-02-18 12:12:33 +0530

    new file:   Network-PortScanner.py

---
+++ Network-PortScanner.py
@@ -0,0 +1,20 @@
+#! /bin/python3
+
+import sys  # module that lets us enter command line arguments and other utils
+import socket
+from datetime import datetime
+
+# Defining the target
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python3 Network-PortScanner.py <ip>")
+    sys.exit()
+
+# Add a banner
+print("-" * 50)
+print("Scanning Target " + target)
+print("Time Started: " + str(datetime.now()))
+print("-" * 50)
