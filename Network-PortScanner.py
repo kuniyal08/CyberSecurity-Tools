49c07ea4ebc5258ac30a6f940db74cc1b80f2117
Kartikeya Uniyal <kartikeyauniyal@protonmail.com>
2026-02-18 13:19:33 +0530

    modified:   Network-PortScanner.py

--- Network-PortScanner.py
+++ Network-PortScanner.py
@@ -18,3 +18,29 @@ print("-" * 50)
 print("Scanning Target " + target)
 print("Time Started: " + str(datetime.now()))
 print("-" * 50)
+
+try:
+    for port in range(50, 85):
+        s = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM
+        )  # AF_INET is IPV4 SOCK_STREAM is the Port
+        socket.setdefaulttimeout(1)  # is a float i.e can be .5
+        result = s.connect_ex(
+            (target, port)
+        )  # returns error indicator. If no error will return 0
+        print("Checking the port {}".format(port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print("\nExiting program. ")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server. ")
+    sys.exit()
